d38693eba5fc228279653be8d44dad85c8a40582
Cy Chan <cychan@lbl.gov>
2022-07-06 16:10:46 -0700

    moved generic QSS Actor and ActorSpace into qss.hxx

--- test/heat/heat.cxx
+++ test/heat/heat.cxx
@@ -12,162 +12,119 @@ using deva::rank_me;
 using deva::rank_me_local;
 using deva::rank_is_local;
 
-///////////
-// Model //
-///////////
+//////////////////////
+// Model Parameters //
+//////////////////////
 
 // model parameters
 const int cell_n = deva::os_env<int>("cells", 100);
-const int cell_per_rank = (cell_n + rank_n-1) / rank_n;
 const double alpha = deva::os_env<double>("alpha", 0.01);
 const double value_threshold = deva::os_env<double>("value_thresh", 0.1);
 
 // local integrator time step
 const Time local_delta_t = deva::os_env<unsigned>("local_delta_t", 1);
-// force advance at least this many seconds
-const Time max_advance_delta_t = deva::os_env<unsigned>("max_advance_delta_t", 100);
-// simulation ends after this many seconds
-const Time sim_end_time = deva::os_env<unsigned>("sim_end_time", 100);
 
-class Cell
+////////////////
+// Cell State //
+////////////////
+
+struct CellState
 {
 public:
 
-  struct State
+  struct NeighborState
   {
     double val = 0;
-  };
-
-  struct Neighbor
-  {
-    State state;
     Time effective = 0;
+
+    // include all object data fields in this macro
+    SERIALIZED_FIELDS(val, effective);
+
+    friend ostream & operator<< (ostream & os, const NeighborState & x);
   };
 
-  Cell () = default;
-  Cell (int i) : id(i) {}
-  Cell (int i, unordered_map<int, Neighbor> nbrs) : id(i), neighbors(std::move(nbrs)) {}
-
-  // helper functions
-  void set_source (double x) { source = x; }
-  void set_last_sent (Neighbor nbr) { last_sent = nbr; }
-  void set_next_advance_time (Time t) { next_advance_time = t; }
-  const State & get_state () const { return state; }
-
-  friend ostream & operator<< (ostream & os, const Cell & x) {
-    os << x.state.val;
-    return os;
-  }
-
-private:
-
-  int id = 0;        // my ID
-  Time time = 0;     // current time
-  State state;       // current state
+  double val = 0;    // value
   double source = 0; // source term
-  unordered_map<int, Neighbor> neighbors; // neighbor states
+
+  // include all object data fields in this macro
+  SERIALIZED_FIELDS(val, source);
 
   // main local integrator function
+  // cur_time and cur_state are in/out parameters
   // integrates from cur_state at cur_time (assuming fixed neighbor states)
   // stops when cur_time reaches end_time or when f(cur_state) is true
   template <class F>
-  void local_integrator (Time & cur_time, Time end_time,
-                         State & cur_state, const F & f) const;
+  static void local_integrator (Time & cur_time, Time end_time,
+                                CellState & cur_state,
+                                const unordered_map<int, NeighborState> & neighbors,
+                                const F & f);
 
-  // overload with no f function
-  void local_integrator (Time & cur_time, Time end_time, State & cur_state) const {
-    local_integrator(cur_time, end_time, cur_state, [] (Time t, const State & s) { return false; });
-  }
+  // returns whether the passed state differs enough from the last sent state to require an update
+  static bool check_need_update (const NeighborState & last_sent, Time t, const CellState & s);
 
-  // last sent value to neighbors
-  Neighbor last_sent;
+  // generate a NeighborState object to share with neighbors
+  NeighborState gen_neighbor_state (Time cur_time) const;
 
-  // next scheduled advance
-  Time next_advance_time = 0;
-
-  // helper functions
-  bool check_need_update (Time t, const State & s) const;
-  const unordered_map<int, Neighbor> get_neighbors () const { return neighbors; }
-  int neighbor_n () const { return neighbors.size(); }
-  pair<Time, State> estimate_next_send_time () const;
-
-public:
-
-  ///////////////////
-  // Advance event //
-  ///////////////////
-
-  struct AdvanceInfo
-  {
-    Time  saved_time = 0;
-    State saved_state;
-  };
-
-  // event that advance's a cell's state to a given time
-  struct Advance
-  {
-    Time event_time = 0;
-    int cell_id = 0;
-
-    // include all object data fields in this macro
-    SERIALIZED_FIELDS(event_time, cell_id);
-    
-    // helper struct to unexecute or commit event
-    struct UC
-    {
-      // saved data to restore state
-      AdvanceInfo adv_info;
-      Neighbor saved_last_sent;
-      Time saved_next_advance_time;
-
-      void unexecute (pdes::event_context & cxt, Advance & me);
-      void commit (pdes::event_context & cxt, Advance & me) {} // does nothing
-    };
-
-    // main event execute function
-    UC execute (pdes::execute_context & cxt);
-  };
-
-  ///////////////////////
-  // ShareState event //
-  ///////////////////////
-
-  // event that sets a destination Cell's neighbor value
-  struct ShareState
-  {
-    Time event_time = 0;
-    int src_cell_id = 0;
-    int dst_cell_id = 0;
-    Neighbor nbr;
-
-    // include all object data fields in this macro
-    SERIALIZED_FIELDS(event_time, src_cell_id, dst_cell_id, nbr);
-    
-    // helper struct to unexecute or commit event
-    struct UC
-    {
-      AdvanceInfo adv_info;
-      Neighbor old_nbr;
-      Time saved_next_advance_time;
-
-      void unexecute (pdes::event_context & cxt, ShareState & me);
-      void commit (pdes::event_context & cxt, ShareState & me) {} // does nothing
-    };
-
-    // main event execute function
-    UC execute (pdes::execute_context & cxt);
-  };
-
-  AdvanceInfo advance (Time t);
-  void unadvance (AdvanceInfo info);
-  Neighbor set_neighbor (int nid, Neighbor nbr);
-  Neighbor send_share_state_events (pdes::execute_context & cxt); // send state update to neighbors
-  Time reschedule (pdes::execute_context & cxt); // self-schedule an advance
+  friend ostream & operator<< (ostream & os, const CellState & x);
 };
 
-//////////////////////
-// Cell Actor Space //
-//////////////////////
+//////////////////////////////
+// CellState Implementation //
+//////////////////////////////
+
+// approximate diffusion for fixed neighbor values
+// integrates from cur_time until end_time OR f() evaluates true
+// this can be made more efficient
+// NOTE: check the math
+template <class F>
+void CellState::local_integrator (Time & cur_time, Time end_time,
+                                  CellState & cur_state,
+                                  const unordered_map<int, NeighborState> & neighbors,
+                                  const F & f)
+{
+  // these are fixed for all steps
+  int nbr_n = neighbors.size();
+  double nbr_sum = 0;
+  for (const auto & p : neighbors) {
+    nbr_sum += p.second.val;
+  }
+
+  // integrate until end time or f() evaluates to true
+  while (cur_time < end_time && !f(cur_time, cur_state)) {
+    Time next_time = std::min(cur_time + local_delta_t, end_time);
+    Time step = next_time - cur_time;
+    double rhs = cur_state.source + alpha * (nbr_sum - nbr_n * cur_state.val);
+    cur_state.val += rhs * step;
+    cur_time = next_time;
+  }
+}
+
+// need to update neighbors when estimated value has changed by threshold
+bool CellState::check_need_update (const NeighborState & last_sent, Time t, const CellState & s)
+{
+  return std::abs(s.val - last_sent.val) >= value_threshold;
+}
+
+typename CellState::NeighborState CellState::gen_neighbor_state (Time cur_time) const
+{
+  return {val, cur_time};
+}
+
+ostream & operator<< (ostream & os, const CellState & x) {
+  os << x.val;
+  return os;
+}
+
+ostream & operator<< (ostream & os, const CellState::NeighborState & x) {
+  os << x.val;
+  return os;
+}
+
+/////////////////////
+// Cell ActorSpace //
+/////////////////////
+
+using Cell = Actor<CellState>;
 
 class CellSpace : public ActorSpace<Cell>
 {
@@ -176,205 +133,44 @@ public:
   {
     // connect cells owned by this rank
     for (int actor_id : rank_to_actor_ids(rank_me())) {
-      unordered_map<int, Cell::Neighbor> neighbors;
+      unordered_map<int, Cell::NeighborState> neighbors;
       // insert left/right neighbors
       if (actor_id == 0) {
         // Dirichlet boundary condition
-        neighbors.insert({actor_id-1, Cell::Neighbor{100, 0}});
+        neighbors.insert({actor_id-1, Cell::NeighborState{100, 0}});
       } else {
-        neighbors.insert({actor_id-1, Cell::Neighbor()});
+        neighbors.insert({actor_id-1, Cell::NeighborState()});
       }
-      neighbors.insert({actor_id+1, Cell::Neighbor()});
-      get_actor(actor_id) = Cell(actor_id, std::move(neighbors));
+      neighbors.insert({actor_id+1, Cell::NeighborState()});
+      get_actor(actor_id).set_neighbors(std::move(neighbors));
     }
   }
 };
 
 CellSpace cs;
 
-///////////////////////////////
-// Cell Class Implementation //
-///////////////////////////////
-
-Cell::Neighbor Cell::set_neighbor (int nid, Cell::Neighbor nbr)
+template <>
+ActorSpace<Cell> * get_actor_space<Cell> ()
 {
-  AllPrint() << "Time " << time << ": Cell " << id << ": set_neighbor() cell " << nid << " to " << nbr.state.val << endl;
-  Neighbor old_nbr = neighbors[nid];
-  neighbors[nid] = nbr;
-  return old_nbr;
+  return static_cast<ActorSpace<Cell> *>(&cs);
 }
 
-// advance cell state to time t
-Cell::AdvanceInfo Cell::advance (Time t)
-{
-  // save old state in case of rollback
-  AdvanceInfo info {time, state};
-  local_integrator(time, t, state);
-  AllPrint() << "Time " << t << ": Cell " << id << ": advance() from time " << info.saved_time << " with value: " << info.saved_state.val << " -> " << state.val << endl;
-  DEVA_ASSERT(time == t);
-  return info;
-}
-
-void Cell::unadvance (AdvanceInfo info)
-{
-  time  = info.saved_time;
-  state = info.saved_state;
-}
-
-// approximate diffusion for fixed neighbor values
-// integrates from cur_time until end_time OR f() evaluates true
-// this can be made more efficient
-// NOTE: check the math
-template <class F>
-void Cell::local_integrator (Time & cur_time, Time end_time,
-                             State & cur_state, const F & f) const
-{
-  // these are fixed for all steps
-  int nbr_n = neighbor_n();
-  double nbr_sum = 0;
-  for (const auto & p : neighbors) {
-    nbr_sum += p.second.state.val;
-  }
-
-  // integrate until end time or f() evaluates to true
-  while (cur_time < end_time && !f(cur_time, cur_state)) {
-    Time next_time = std::min(cur_time + local_delta_t, end_time);
-    Time step = next_time - cur_time;
-    double rhs = source + alpha * (nbr_sum - nbr_n * cur_state.val);
-    cur_state.val += rhs * step;
-    cur_time = next_time;
-  }
-}
-
-// need to update neighbors when estimated value has changed by threshold
-bool Cell::check_need_update (Time t, const State & s) const
-{
-  return std::abs(s.val - last_sent.state.val) >= value_threshold;
-}
-
-// compute when to send updated value to neighbors
-pair<Time, Cell::State> Cell::estimate_next_send_time () const
-{
-  Time est_time = time;
-  State est_state = state;
-  auto checker = [this] (Time t, const State & s) {
-    return check_need_update(t, s);
-  };
-
-  // estimate when we would need to update neighbors via local integration
-  local_integrator(est_time, time + max_advance_delta_t, est_state, checker);
-
-  return {est_time, est_state};
-}
-
-Time Cell::reschedule (pdes::execute_context & cxt)
-{
-  Time saved_next_advance_time = next_advance_time;
-  if (next_advance_time <= time) {
-    next_advance_time = std::numeric_limits<Time>::max();
-  }
-
-  // estimate when value will change enough to send to neighbors
-  Time est_time; State est_state;
-  tie(est_time, est_state) = estimate_next_send_time();
-
-  // only reschedule if est_time is earlier than currently scheduled advance 
-  if (est_time > time && est_time < std::min(next_advance_time, sim_end_time)) {
-    int rank = cs.actor_id_to_rank(id);
-    int cd   = cs.actor_id_to_idx(id);
-    AllPrint() << "Time " << time << ": Cell " << id << ": reschedule() for future time: " << est_time << endl;
-    cxt.send(rank, cd, get_timestamp(est_time, 0), Advance {est_time, id});
-    next_advance_time = est_time;
-  }
-
-  return saved_next_advance_time;
-}
-
-Cell::Neighbor Cell::send_share_state_events (pdes::execute_context & cxt)
-{
-  Neighbor saved_last_sent = last_sent;
-
-  if (check_need_update(time, state)) {
-    AllPrint() << "Time " << time << ": Cell " << id << ": send_share_state_events() with value: " << state.val << endl;
-    Neighbor nbr{state, time};
-    for (const auto & p : get_neighbors()) {
-      int nbr_id = p.first;
-      if (cs.has_actor(nbr_id)) {
-        int nbr_rank = cs.actor_id_to_rank(nbr_id);
-        int nbr_cd   = cs.actor_id_to_idx(nbr_id);
-        ShareState event{time, id, nbr_id, nbr};
-        cxt.send(nbr_rank, nbr_cd, get_timestamp(time, 1), event);
-      }
-    }
-    last_sent = nbr;
-  }
-
-  return saved_last_sent;
-}
-
-/////////////////////////////////////
-// ShareState Event Implementation //
-/////////////////////////////////////
-
-Cell::ShareState::UC Cell::ShareState::execute (pdes::execute_context & cxt)
-{
-  Cell & cell = cs.get_actor(dst_cell_id);
-  // advance to given time using old neighbor values
-  AdvanceInfo adv_info = cell.advance(event_time);
-  // set the neighbor with new values from event
-  Neighbor old_nbr = cell.set_neighbor(src_cell_id, nbr);
-  DEVA_ASSERT(nbr.effective > old_nbr.effective);
-  // reschedule advance given new neighbor values
-  Time saved_next_advance_time = cell.reschedule(cxt);
-
-  return ShareState::UC {adv_info, old_nbr, saved_next_advance_time};
-}
-
-void Cell::ShareState::UC::unexecute (pdes::event_context & cxt, Cell::ShareState & me)
-{
-  // restore the old state
-  Cell & cell = cs.get_actor(me.dst_cell_id);
-  cell.unadvance(std::move(adv_info));
-  cell.set_neighbor(me.src_cell_id, old_nbr);
-  cell.set_next_advance_time(saved_next_advance_time);
-}
-
-//////////////////////////////////
-// Advance Event Implementation //
-//////////////////////////////////
-
-Cell::Advance::UC Cell::Advance::execute (pdes::execute_context & cxt)
-{
-  Cell & cell = cs.get_actor(cell_id);
-  AdvanceInfo adv_info = cell.advance(event_time);
-  // share state with neighbors
-  Neighbor saved_last_sent = cell.send_share_state_events(cxt);
-  // reschedule next advance
-  Time saved_next_advance_time = cell.reschedule(cxt);
-  return Advance::UC {std::move(adv_info), saved_last_sent, saved_next_advance_time};
-}
-
-void Cell::Advance::UC::unexecute (pdes::event_context & cxt, Cell::Advance & me) {
-  // restore the old state
-  Cell & cell = cs.get_actor(me.cell_id);
-  cell.unadvance(std::move(adv_info));
-  cell.set_last_sent(saved_last_sent);
-  cell.set_next_advance_time(saved_next_advance_time);
-};
-
-////////////////
-// Simulation //
-////////////////
+/////////////////////
+// Main Simulation //
+/////////////////////
 
 // this function runs in parallel on every rank
 void rank_main ()
 {
-  pdes::init(cell_per_rank);
   cs.init(cell_n);
   cs.connect();
+
   cs.print();
+
+  pdes::init(cs.actor_per_rank());
   cs.root_events();
   pdes::drain();
+
   cs.print();
 }
 
--- test/heat/qss.hxx
+++ test/heat/qss.hxx
@@ -3,6 +3,7 @@
 #include <devastator/diagnostic.hxx>
 #include <devastator/world.hxx>
 #include <devastator/pdes.hxx>
+#include <devastator/os_env.hxx>
 
 #include <cstdint>
 #include <vector>
@@ -10,6 +11,11 @@
 
 #include "time.hxx"
 #include "print.hxx"
+
+// force advance at least this many seconds
+const Time max_advance_delta_t = deva::os_env<unsigned>("max_advance_delta_t", 100);
+// simulation ends after this many seconds
+const Time sim_end_time = deva::os_env<unsigned>("sim_end_time", 100);
 
 template <class Actor>
 class ActorSpace
@@ -30,9 +36,13 @@ public:
     deva::barrier();
   }
 
+  int actor_per_rank () const {
+    return (actor_n + deva::rank_n-1) / deva::rank_n;
+  }
+
   virtual void connect () {}
 
-  void root_events ()
+  void root_events () const
   {
     // insert a root advance event for each actor owned by this rank
     for (int actor_id : rank_to_actor_ids(deva::rank_me())) {
@@ -50,21 +60,21 @@ public:
   }
 
   // which rank a actor is assigned to
-  int actor_id_to_rank (int actor_id)
+  int actor_id_to_rank (int actor_id) const
   {
     DEVA_ASSERT(has_actor(actor_id));
     return actor_id / actor_per_rank();
   }
 
   // returns actor's index within rank
-  int actor_id_to_idx (int actor_id)
+  int actor_id_to_idx (int actor_id) const
   {
     DEVA_ASSERT(has_actor(actor_id));
     return actor_id % actor_per_rank();
   }
 
   // which actors are assigned to a rank
-  std::vector<int> rank_to_actor_ids (int rank)
+  std::vector<int> rank_to_actor_ids (int rank) const
   {
     int lb = std::min(actor_n,  deva::rank_me()      * actor_per_rank());
     int ub = std::min(actor_n, (deva::rank_me() + 1) * actor_per_rank());
@@ -104,7 +114,7 @@ public:
         if (!flag_first) {
           Print() << ',';
         }
-        Print() << actors.at(actor_id);
+        Print() << actors.at(actor_id).get_state();
         flag_first = false;
       }
       Print() << ']' << std::endl;
@@ -118,8 +128,283 @@ private:
 
   // here's where the actor actors are stored
   std::unordered_map<int, Actor> actors;
+};
 
-  int actor_per_rank () const {
-    return (actor_n + deva::rank_n-1) / deva::rank_n;
+// specialize this function to return the Actor's ActorSpace
+template <class Actor>
+ActorSpace<Actor> * get_actor_space ()
+{
+  return nullptr;
+}
+
+/////////////////
+// Actor Class //
+/////////////////
+
+template <class State>
+class Actor
+{
+public:
+
+  using NeighborState = typename State::NeighborState;
+
+  Actor () = default;
+  Actor (int i) : id(i) {}
+
+  ///////////////////
+  // Advance event //
+  ///////////////////
+
+  struct AdvanceInfo
+  {
+    Time  saved_time = 0;
+    State saved_state;
+  };
+
+  // event that advance's a actor's state to a given time
+  struct Advance
+  {
+    Time event_time = 0;
+    int actor_id = 0;
+
+    // include all object data fields in this macro
+    SERIALIZED_FIELDS(event_time, actor_id);
+
+    // helper struct to unexecute or commit event
+    struct UC
+    {
+      // saved data to restore state
+      AdvanceInfo adv_info;
+      NeighborState saved_last_sent;
+      Time saved_next_advance_time;
+
+      void unexecute (deva::pdes::event_context & cxt, Advance & me);
+      void commit (deva::pdes::event_context & cxt, Advance & me) {} // does nothing
+    };
+
+    // main event execute function
+    UC execute (deva::pdes::execute_context & cxt);
+  };
+
+  ///////////////////////
+  // ShareState event //
+  ///////////////////////
+
+  // event that sets a destination Actor's neighbor value
+  struct ShareState
+  {
+    Time event_time = 0;
+    int src_actor_id = 0;
+    int dst_actor_id = 0;
+    NeighborState nbr;
+
+    // include all object data fields in this macro
+    SERIALIZED_FIELDS(event_time, src_actor_id, dst_actor_id, nbr);
+
+    // helper struct to unexecute or commit event
+    struct UC
+    {
+      AdvanceInfo adv_info;
+      NeighborState old_nbr;
+      Time saved_next_advance_time;
+
+      void unexecute (deva::pdes::event_context & cxt, ShareState & me);
+      void commit (deva::pdes::event_context & cxt, ShareState & me) {} // does nothing
+    };
+
+    // main event execute function
+    UC execute (deva::pdes::execute_context & cxt);
+  };
+
+  ////////////////////
+  // Public methods //
+  ////////////////////
+
+  AdvanceInfo advance (Time t);
+  void unadvance (AdvanceInfo info);
+  NeighborState set_neighbor (int nid, NeighborState nbr);
+  NeighborState send_share_state_events (deva::pdes::execute_context & cxt); // send state update to neighbors
+  Time reschedule (deva::pdes::execute_context & cxt); // self-schedule an advance
+
+  const State & get_state () const { return state; }
+  void set_neighbors(std::unordered_map<int, NeighborState> nbrs) { neighbors = std::move(nbrs); }
+  void set_last_sent (NeighborState nbr) { last_sent = nbr; }
+  void set_next_advance_time (Time t) { next_advance_time = t; }
+
+private:
+
+  /////////////////////////////////
+  // Private members and methods //
+  /////////////////////////////////
+
+  int id = 0;    // my ID
+  Time time = 0; // current time
+  State state;   // current state
+  std::unordered_map<int, NeighborState> neighbors; // neighbor states
+  NeighborState last_sent; // last sent value to neighbors
+  Time next_advance_time = 0; // next scheduled advance
+
+  // include all object data fields in this macro
+  SERIALIZED_FIELDS(id, time, state, neighbors, last_sent, next_advance_time);
+
+  // helper functions
+  std::pair<Time, State> estimate_next_send_time () const;
+};
+
+// advance cell state to time t
+template <class State>
+typename Actor<State>::AdvanceInfo
+Actor<State>::advance (Time t)
+{
+  // save old state in case of rollback
+  AdvanceInfo info {time, state};
+
+  auto always_false = [] (Time t, const State & s) { return false; };
+  state.local_integrator(time, t, state, neighbors, always_false);
+  AllPrint() << "Time " << t << ": Actor " << id << ": advance() from time " << info.saved_time << " with value: " << info.saved_state << " -> " << state << std::endl;
+  DEVA_ASSERT(time == t);
+  return info;
+}
+
+template <class State>
+void Actor<State>::unadvance (AdvanceInfo info)
+{
+  time  = info.saved_time;
+  state = info.saved_state;
+}
+
+template <class State>
+typename State::NeighborState
+Actor<State>::set_neighbor (int nid, typename State::NeighborState nbr)
+{
+  AllPrint() << "Time " << time << ": Actor " << id << ": set_neighbor() cell " << nid << " to " << nbr << std::endl;
+  NeighborState old_nbr = neighbors[nid];
+  neighbors[nid] = nbr;
+  return old_nbr;
+}
+
+// compute when to send updated value to neighbors
+template <class State>
+std::pair<Time, State> Actor<State>::estimate_next_send_time () const
+{
+  Time est_time = time;
+  State est_state = state;
+  auto checker = [this] (Time t, const State & s) {
+    return state.check_need_update(last_sent, t, s);
+  };
+
+  // estimate when we would need to update neighbors via local integration
+  state.local_integrator(est_time, time + max_advance_delta_t, est_state, neighbors, checker);
+
+  return {est_time, est_state};
+}
+
+template <class State>
+typename State::NeighborState
+Actor<State>::send_share_state_events (deva::pdes::execute_context & cxt)
+{
+  NeighborState saved_last_sent = last_sent;
+
+  if (state.check_need_update(last_sent, time, state)) {
+    AllPrint() << "Time " << time << ": Actor " << id << ": send_share_state_events() with value: " << state << std::endl;
+    NeighborState nbr = state.gen_neighbor_state(time);
+    const auto & as = *get_actor_space<Actor<State>>();
+    for (const auto & p : neighbors) {
+      int nbr_id = p.first;
+      if (as.has_actor(nbr_id)) {
+        int nbr_rank = as.actor_id_to_rank(nbr_id);
+        int nbr_cd   = as.actor_id_to_idx(nbr_id);
+        ShareState event{time, id, nbr_id, nbr};
+        cxt.send(nbr_rank, nbr_cd, get_timestamp(time, 1), event);
+      }
+    }
+    last_sent = nbr;
   }
+
+  return saved_last_sent;
+}
+
+template <class State>
+Time Actor<State>::reschedule (deva::pdes::execute_context & cxt)
+{
+  Time saved_next_advance_time = next_advance_time;
+  if (next_advance_time <= time) {
+    next_advance_time = std::numeric_limits<Time>::max();
+  }
+
+  // estimate when value will change enough to send to neighbors
+  Time est_time; State est_state;
+  std::tie(est_time, est_state) = estimate_next_send_time();
+
+  // only reschedule if est_time is earlier than currently scheduled advance 
+  if (est_time > time && est_time < std::min(next_advance_time, sim_end_time)) {
+    const auto & as = *get_actor_space<Actor<State>>();
+    int rank = as.actor_id_to_rank(id);
+    int cd   = as.actor_id_to_idx(id);
+    AllPrint() << "Time " << time << ": Actor " << id << ": reschedule() for future time: " << est_time << std::endl;
+    cxt.send(rank, cd, get_timestamp(est_time, 0), Advance {est_time, id});
+    next_advance_time = est_time;
+  }
+
+  return saved_next_advance_time;
+}
+
+/////////////////////////////////////
+// ShareState Event Implementation //
+/////////////////////////////////////
+
+template <class State>
+typename Actor<State>::ShareState::UC
+Actor<State>::ShareState::execute (deva::pdes::execute_context & cxt)
+{
+  auto & as = *get_actor_space<Actor<State>>();
+  Actor<State> & cell = as.get_actor(dst_actor_id);
+  // advance to given time using old neighbor values
+  AdvanceInfo adv_info = cell.advance(event_time);
+  // set the neighbor with new values from event
+  NeighborState old_nbr = cell.set_neighbor(src_actor_id, nbr);
+  DEVA_ASSERT(nbr.effective > old_nbr.effective);
+  // reschedule advance given new neighbor values
+  Time saved_next_advance_time = cell.reschedule(cxt);
+
+  return ShareState::UC {adv_info, old_nbr, saved_next_advance_time};
+}
+
+template <class State>
+void Actor<State>::ShareState::UC::unexecute (deva::pdes::event_context & cxt, Actor<State>::ShareState & me)
+{
+  // restore the old state
+  auto & as = *get_actor_space<Actor<State>>();
+  Actor<State> & cell = as.get_actor(me.dst_actor_id);
+  cell.unadvance(std::move(adv_info));
+  cell.set_neighbor(me.src_actor_id, old_nbr);
+  cell.set_next_advance_time(saved_next_advance_time);
+}
+
+//////////////////////////////////
+// Advance Event Implementation //
+//////////////////////////////////
+
+template <class State>
+typename Actor<State>::Advance::UC
+Actor<State>::Advance::execute (deva::pdes::execute_context & cxt)
+{
+  auto & as = *get_actor_space<Actor<State>>();
+  Actor<State> & cell = as.get_actor(actor_id);
+  AdvanceInfo adv_info = cell.advance(event_time);
+  // share state with neighbors
+  NeighborState saved_last_sent = cell.send_share_state_events(cxt);
+  // reschedule next advance
+  Time saved_next_advance_time = cell.reschedule(cxt);
+  return Advance::UC {std::move(adv_info), saved_last_sent, saved_next_advance_time};
+}
+
+template <class State>
+void Actor<State>::Advance::UC::unexecute (deva::pdes::event_context & cxt, Actor<State>::Advance & me) {
+  // restore the old state
+  auto & as = *get_actor_space<Actor<State>>();
+  Actor<State> & cell = as.get_actor(me.actor_id);
+  cell.unadvance(std::move(adv_info));
+  cell.set_last_sent(saved_last_sent);
+  cell.set_next_advance_time(saved_next_advance_time);
 };
