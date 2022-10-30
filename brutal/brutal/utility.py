6b94a08eb65559d6c646e808e98516057a6ac0e5
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-30 13:34:46 +0000

    Adding tarfile member sanitization to extractall()

--- brutal/brutal/utility.py
+++ brutal/brutal/utility.py
@@ -43,7 +43,29 @@ def untar(path_tar):
   import tarfile
   with tarfile.open(path_tar) as f:
     inner_dir = opsys.path.join(untar_dir, f.members[0].name)
-    f.extractall(untar_dir)
+    
+    import os
+    
+    def is_within_directory(directory, target):
+        
+        abs_directory = os.path.abspath(directory)
+        abs_target = os.path.abspath(target)
+    
+        prefix = os.path.commonprefix([abs_directory, abs_target])
+        
+        return prefix == abs_directory
+    
+    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+    
+        for member in tar.getmembers():
+            member_path = os.path.join(path, member.name)
+            if not is_within_directory(path, member_path):
+                raise Exception("Attempted Path Traversal in Tar File")
+    
+        tar.extractall(path, members, numeric_owner=numeric_owner) 
+        
+    
+    safe_extract(f, untar_dir)
   return inner_dir
 
 @memodb.traced
